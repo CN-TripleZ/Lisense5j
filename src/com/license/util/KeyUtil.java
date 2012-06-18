4a97ab74ec95498b96a5772b580a063b4e9cd6fe
jeffreyzhang <shine8606@gmail.com>
2012-06-18 22:09:01 +0800

    keyutil

--- src/com/license/util/KeyUtil.java
+++ src/com/license/util/KeyUtil.java
@@ -1,36 +1,54 @@
 package com.license.util;
 
+import java.io.UnsupportedEncodingException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.SecureRandom;
-
-import com.sun.xml.internal.messaging.saaj.util.Base64;
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * @author jeffreyzhang
  * 
  */
 public class KeyUtil {
-	private byte[] priKey;
-	private byte[] pubKey;
 
-	public void generateKey() {
+	public static KeyPair generateKey(String key) {
 		try {
 			KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
-			SecureRandom secrand = new SecureRandom();
-			secrand.setSeed("jeffreyzhang".getBytes()); // 初始化随机产生器
-			keygen.initialize(1024, secrand);
-			KeyPair keys = keygen.genKeyPair();
-			PublicKey pubkey = keys.getPublic();
-			PrivateKey prikey = keys.getPrivate();
+			SecureRandom random = new SecureRandom();
+			random.setSeed(key.getBytes());
 
-			priKey = Base64.encode(prikey.getEncoded());
-			pubKey = Base64.encode(pubkey.getEncoded());
+			keygen.initialize(1024, random);
+
+			KeyPair keys = keygen.generateKeyPair();
+			return keys;
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
+		return null;
+	}
+
+	public static String byte2hex(byte[] b) {
+		String hs = "";
+		String stmp = "";
+		for (int n = 0; n < b.length; n++) {
+			stmp = (java.lang.Integer.toHexString(b[n] & 0XFF));
+			if (stmp.length() == 1) {
+				hs = hs + "0" + stmp;
+			} else {
+				hs = hs + stmp;
+			}
+		}
+		return hs.toUpperCase();
+	}
+
+	public static void main(String[] args) throws NoSuchAlgorithmException,
+			InvalidKeySpecException, UnsupportedEncodingException {
+		KeyPair key = KeyUtil.generateKey("jeffreyzhang");
+		String pri = KeyUtil.byte2hex(key.getPrivate().getEncoded());
+		String pub = KeyUtil.byte2hex(key.getPublic().getEncoded());
+		System.out.println(pri);
+		System.out.println(pub);
 	}
 }
